a29fbb99dcd341669ecbe56718b87a11dbd96808
xuantruongcox <truongcool999@gmail.com>
2023-07-27 16:00:19 +0700

    FIX

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 exports.handler = async function(event, context){
-    console.log("ENVIRONMENT VARAIABLES\n" + JSON.stringify(process.env, null, 2));
-    console.log("EVENT\n" + JSON.stringify(event, null, 2));
-    return context.logStreamName
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+    return context.logStreamName;
 }
